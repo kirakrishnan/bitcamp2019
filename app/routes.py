386dffe7fce0b5529d8241ff03ab71b57fce7474
kirakrishnan <aravindakrishnan92@gmail.com>
2019-04-13 21:06:06 -0400

    adding logic for generating graph

--- app/routes.py
+++ app/routes.py
@@ -18,3 +18,29 @@ def result():
             result[k]=v
         result["TAKEHOME"] = salary
         return render_template("result.html", result = result)
+
+
+import random
+from io import BytesIO
+# from StringIO import StringIO  # python 2.7x
+
+from flask import Flask, make_response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+@app.route('/plot.png')
+def plot():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = BytesIO()
+    # output = StringIO()  # python 2.7x
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
